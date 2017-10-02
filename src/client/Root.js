3d965b06ae2a1c3d5b4cc34a5702ffb355c9d0d8
Owen Nicol <owennicol@gmail.com>
2017-10-02 14:15:16 +0100

    Introduce redux

--- src/client/Root.js
+++ src/client/Root.js
@@ -1,11 +1,39 @@
 import React from 'react'
-import Router from 'react-router/lib/Router'
-import browserHistory from 'react-router/lib/browserHistory'
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import thunk from 'redux-thunk'
 import routes from '../routes'
+
+// Redux setup
+const initialState = (window.state) ? JSON.parse(window.state) : {}
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+const reducers = require('../reducers')
+
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    routing: routerReducer
+  }),
+  initialState,
+  composeEnhancers(
+    applyMiddleware(
+      thunk
+    )
+  )
+)
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
 
 // We need a Root component for React Hot Loading.
 function Root () {
-  return <Router history={browserHistory} routes={routes} />
+  return (
+    <Provider store={store}>
+      <Router history={history} routes={routes} />
+    </Provider>
+  )
 }
 
 export default Root
